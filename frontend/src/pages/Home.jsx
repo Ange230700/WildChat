d6a4808260d63592805d3d4606d3892594137986
Ange230700 <kouakouangeericstephane@gmail.com>
2024-01-31 17:49:22 +0100

    working on WildChat...

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,11 @@ import axios from "axios";
 
 function Home() {
   const [wilders, setWilders] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  function handleSearchChange(event) {
+    setSearchValue(event.target.value);
+  }
 
   useEffect(() => {
     axios
@@ -28,6 +33,8 @@ function Home() {
             type="search"
             placeholder="Search"
             className="SearchBarInput"
+            value={searchValue || ""}
+            onChange={handleSearchChange}
           />
         </div>
         <section className="TabsWrapper">
@@ -39,27 +46,31 @@ function Home() {
           </div>
         </section>
         <div className="WildersWrapper">
-          {wilders.map((wilder) => (
-            <div className="WilderContainer">
-              <div className="AvatarContainer">
-                <img
-                  className="Avatar"
-                  src={`${
-                    import.meta.env.VITE_BACKEND_URL
-                  }/assets/images/89375707.png`}
-                  alt="Avatar"
-                />
-              </div>
-              <div className="WilderInfo">
-                <div className="WilderNameContainer">
-                  <h2 className="WilderName">{wilder.username}</h2>
+          {wilders
+            .filter((wilder) =>
+              wilder.username.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((wilder) => (
+              <div className="WilderContainer">
+                <div className="AvatarContainer">
+                  <img
+                    className="Avatar"
+                    src={`${
+                      import.meta.env.VITE_BACKEND_URL
+                    }/assets/images/89375707.png`}
+                    alt="Avatar"
+                  />
                 </div>
-                <div className="WilderBioContainer">
-                  <h3 className="WilderBio">{wilder.bio}</h3>
+                <div className="WilderInfo">
+                  <div className="WilderNameContainer">
+                    <h2 className="WilderName">{wilder.username}</h2>
+                  </div>
+                  <div className="WilderBioContainer">
+                    <h3 className="WilderBio">{wilder.bio}</h3>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
         <nav className="Navbar">
           <div className="Icon">
