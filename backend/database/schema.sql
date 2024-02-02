f2cc3cc978377358be6575138a1eb369e86d9bbe
Ange230700 <kouakouangeericstephane@gmail.com>
2024-02-02 11:19:00 +0100

    working on WildChat...

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,10 +1,30 @@
 DROP TABLE IF EXISTS `User`;
 
--- TODO: Add `avatar_id` column to `User` table and create `Avatar` table with `id` and `url` columns to store user avatars in the database.
-
 CREATE TABLE `User` (
   `id` INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
   `username` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
   `hashed_password` VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS `Message`;
+
+CREATE TABLE `Message` (
+  `id` INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+  `sender_id` INT NOT NULL,
+  `receiver_id` INT NOT NULL,
+  `content` TEXT NOT NULL,
+  FOREIGN KEY (sender_id) REFERENCES User(id) ON DELETE CASCADE,
+  FOREIGN KEY (receiver_id) REFERENCES User(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS `Chat_session`;
+
+CREATE TABLE `Chat_session` (
+  `id` INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+  `user1_id` INT NOT NULL,
+  `user2_id` INT NOT NULL,
+  `last_message_id` INT, 
+  FOREIGN KEY (user1_id) REFERENCES User(id) ON DELETE CASCADE,
+  FOREIGN KEY (user2_id) REFERENCES User(id) ON DELETE CASCADE
 );
